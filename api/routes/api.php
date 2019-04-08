5c51a896e0da8168626ba10fd49afe705ef4f261
Kier Borromeo <seraphipod@gmail.com>
2019-04-09 00:42:16 +0800

    Fix assigned method to update profile route

--- api/routes/api.php
+++ api/routes/api.php
@@ -17,7 +17,7 @@ Route::post('/register', 'MeController@register');
 
 Route::middleware('auth:api')->group(function() {
     Route::get('/me', 'MeController@me');
-    Route::put('/me', 'MeController@profile');
+    Route::put('/me', 'MeController@update');
     Route::put('/me/password', 'MeController@password');
     Route::put('/me/avatar', 'MeController@avatar');
     
