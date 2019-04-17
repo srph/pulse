e927888e063617c0ce1f1c5c94942d4c613406a8
Kier Borromeo <seraphipod@gmail.com>
2019-04-17 11:28:02 +0800

    Redesign tracker

--- ui/src/screens/main.dashboard.tracker.home/index.tsx
+++ ui/src/screens/main.dashboard.tracker.home/index.tsx
@@ -7,7 +7,6 @@ import getTrackerYear from '~/utils/tracker/getTrackerYear'
 import getEntry from '~/utils/tracker/getEntry'
 import { ClonedProps } from '~/screens/main.dashboard.tracker/types'
 import LabelMenu from './LabelMenu'
-import color from 'color'
 import s from '~/styles'
 
 const C = {} as any
@@ -16,149 +15,152 @@ C.wrapper = css`
   padding-bottom: 64px;
 `
 C.calendar = css`
-  display: flex;
   width: 100%;
   margin-right: 16px;
 `
-C.calendarColumn = css`
-  width: ${(1 / 13) * 100}%;
-`
-C.calendarBox = css`
-  box-sizing: content-box;
-  border: 1px solid ${s['color-bw-400']};
+C.heading = css`
+  position: sticky;
+  top: 0;
+  z-index: 500;
+  display: flex;
+  width: 100%;
+  margin-bottom: 16px;
+  padding-top: 16px;
+  padding-bottom: 16px;
   background: ${s['color-bw-100']};
-
-  &:first-child {
-    position: sticky;
-    top: 0;
-    z-index: 500;
-  }
-
-  /* .css-${C.calendarColumn.name}:not(:last-child) & {
-    border-right-color: transparent;
-  } */
-
-  /* &:not(:last-child) {
-    border-bottom-color: transparent;
-  } */
+  border-bottom: 1px solid ${s['color-bw-200']};
 `
-C.calendarBoxIsToday = css`
-  border-color: ${s['color-blue-500']};
-`
-C.calendarBoxInner = css`
-  position: relative;
-  padding-bottom: 100%;
-`
-C.calendarBoxContent = css`
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  display: block;
-  height: 100%;
-  width: 100%;
-  padding: 0;
-  border: 0;
-  background: transparent;
-  outline: 0;
-
-  &:focus {
-    border-color: ${s['color-blue-400']};
-    box-shadow: 0 0 0 3px ${s['color-blue-300']};
-  }
-`
-C.calendarBoxContentIsButton = css`
-  cursor: pointer;
-`
-C.calendarBoxContentTodayBorder = css`
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  border: 1px solid ${s['color-blue-500']};
-  border-top-width: 5px;
-  pointer-events: none;
-`
-C.calendarBoxTitle = css`
-  height: 100%;
-  width: 100%;
+C.headingColumn = css`
   display: flex;
   align-items: center;
   justify-content: center;
+  width: ${(1 / 13) * 100}%;
+  padding-left: 4px;
+  padding-right: 4px;
+  font-weight: 800;
   text-transform: uppercase;
-  font-weight: 600;
+  color: ${s['color-bw-700']};
 `
-C.calendarBoxLabel = css`
-  position: absolute;
-  top: 16px;
-  right: 16px;
+C.body = css`
   display: flex;
-  align-items: center;
-  justify-content: center;
-  height: calc(100% - 32px);
-  width: calc(100% - 32px);
-  border-radius: ${s['border-radius']}px;
+  margin-left: -4px;
+  margin-right: -4px;
 `
-C.calendarBoxLabelBeforeCreationIndicator = css`
+C.bodyColumn = css`
+  width: ${(1 / 13) * 100}%;
+  padding-left: 4px;
+  padding-right: 4px;
+`
+C.boxContainer = css`
+  position: relative;
+  width: 100%;
+  padding-bottom: 100%;
+  margin-bottom: 8px;
+`
+C.boxInner = css`
   position: absolute;
   top: 0;
   left: 0;
   right: 0;
   bottom: 0;
-  background-image: url("data:image/svg+xml,%3Csvg width='6' height='6' viewBox='0 0 6 6' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='%239C92AC' fill-opacity='0.4' fill-rule='evenodd'%3E%3Cpath d='M5 0h1L0 6V5zM6 5v1H5z'/%3E%3C/g%3E%3C/svg%3E");
-  background-repeat: repeat;
-  background-size: 1px;
-  clip-path: polygon(100% 0, 0% 100%, 100% 100%);
 `
-C.calendarBoxLabelInactiveIndicator = css`
-  position: absolute;
-  top: 16px;
-  right: 16px;
+C.date = css`
   display: flex;
   align-items: center;
   justify-content: center;
-  height: calc(100% - 32px);
-  width: calc(100% - 32px);
-  border-radius: ${s['border-radius']}px;
-  background: url("data:image/svg+xml,%3Csvg width='6' height='6' viewBox='0 0 6 6' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='%239C92AC' fill-opacity='0.4' fill-rule='evenodd'%3E%3Cpath d='M5 0h1L0 6V5zM6 5v1H5z'/%3E%3C/g%3E%3C/svg%3E");
+  height: 100%;
+  width: 100%;
+  font-weight: 800;
+  color: ${s['color-bw-700']};
 `
-C.calendarBoxLabelStamp = css`
-  height: 28px;
-  width: 28px;
+C.box = css`
+  height: 100%;
+  width: 100%;
+  border-radius: ${s['border-radius']}px;
+  border: 1px solid ${s['color-bw-500']};
+`
+C.boxIsBeforeCreation = css`
+  border-color: ${s['color-bw-200']};
+`
+C.boxIsDisabled = css`
+  background: ${s['color-bw-400']};
+  border-color: ${s['color-bw-400']};
+`
+C.today = css`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  text-align: center;
+  font-size: 10px;
+  border: 2px solid ${s['color-blue-500']};
+  border-radius: ${s['border-radius']}px;
+`
+C.todayText = css`
+  height: 18px;
+  text-align: center;
+  padding-top: 4px;
+  color: ${s['color-bw-100']};
+  font-weight: 800;
+  letter-spacing: 2px;
+  background: ${s['color-blue-500']};
+  text-transform: uppercase;
+`
+C.entry = css`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  height: 32px;
+  width: 32px;
+  border-radius: ${s['border-radius']}px;
+  transform: translateY(-50%) translateX(-50%);
+`
+C.entryIsToday = css` 
+  top: calc(50% + 9px);
+`
+C.fill = css`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0;
+  background: transparent;
+  border: 0;
+  height: 100%;
+  width: 100%;
+  cursor: pointer;
+  outline: 0;
+`
+C.fillCircle = css`
+  height: 18px;
+  width: 18px;
   border-radius: 50%;
   opacity: 0;
 
-  .css-${C.calendarBox.name}:hover & {
+  .css-${C.fill.name}:hover & {
     opacity: 1;
   }
-`
-C.calendarBoxLabelInvalid = css`
-  color: red;
-  opacity: 0;
-
-  .css-${C.calendarBox.name}:hover & {
-    opacity: 1;
-  }
-`
-C.calendarBoxLabelIsToday = css`
-  border-top-width: 5px;
-  border-color: ${s['color-blue-500']};
 `
 
 const columns = Array(13).fill(0)
-const boxes = Array(32).fill(0)
+const boxes = Array(31).fill(0)
 
 class DashboardTrackerHome extends React.Component<ClonedProps, {}> {
   render() {
     const { tracker } = this.props
 
     if (tracker == null) {
-      return null;
+      return null
     }
 
     const creationDate: Date = parse(tracker.created_at)
+    const year = getTrackerYear(tracker)
+    const today = new Date()
 
     return (
       <div>
@@ -166,83 +168,74 @@ class DashboardTrackerHome extends React.Component<ClonedProps, {}> {
 
         <div css={C.wrapper}>
           <div css={C.calendar}>
-            {columns.map((_, columnIndex: number) => {
-              return (
-                <div css={C.calendarColumn} key={columnIndex} data-column>
-                  {boxes.map((_, boxIndex: number) => {
-                    const isHeading = columnIndex > 0 && boxIndex === 0
-                    const isDate = columnIndex === 0 && boxIndex > 0
-                    const isEntry = columnIndex > 0 && boxIndex > 0
-                    const year = getTrackerYear(this.props.tracker)
+            <div css={C.heading}>
+              {columns.map((_, i) => (
+                <div css={C.headingColumn} key={i}>
+                  {i > 0 && format(new Date(year, i - 1), 'MMM')}
+                </div>
+              ))}
+            </div>
 
-                    if (isEntry && boxIndex > getDaysInMonth(new Date(year, columnIndex - 1))) {
-                      return
-                    }
+            <div css={C.body}>
+              {columns.map((_, i) => (
+                <div css={C.bodyColumn} key={i}>
+                  {boxes.map((_, j) => {
+                    const date = new Date(year, i - 1, j)
+                    const isDateToday = isToday(date)
+                    const isDateBeforeCreationDate = isBefore(date, creationDate)
+                    const isDateBeforeToday = isBefore(date, today)
+                    const isBeforeOrToday = isDateBeforeToday || isDateToday
 
-                    const isToday2 = isToday(new Date(year, columnIndex - 1, boxIndex))
-                    const isEntryToday = isEntry && isToday2
                     const entry = getEntry({
                       tracker: this.props.tracker,
-                      month: columnIndex,
-                      day: boxIndex
+                      month: i,
+                      day: j + 1
                     })
-                    const date = new Date(year, columnIndex - 1, boxIndex)
-                    const isBeforeCreationDate = isBefore(date, creationDate)
-                    const isBeforeOrToday = isBefore(date, new Date()) || isToday2
-                    const isActive = isEntry && isBeforeOrToday
-                    const Content = !isActive ? 'div' : 'button'
 
                     return (
-                      <div css={[C.calendarBox, isEntryToday && C.calendarBoxIsToday]} data-calendar-box key={boxIndex}>
-                        <div css={C.calendarBoxInner}>
-                          <Content
-                            css={[C.calendarBoxContent, isActive && C.calendarBoxContentIsButton]}
-                            onClick={isActive ? () => this.props.onEntryClick(columnIndex, boxIndex) : () => { }}
-                            data-calendar-etits>
-                            {isHeading && (
-                              <div css={C.calendarBoxTitle}>{format(new Date(year, columnIndex - 1), 'MMM')}</div>
-                            )}
+                      <div css={C.boxContainer} key={j}>
+                        <div css={C.boxInner}>
+                          {i === 0 && (
+                            <div css={C.date} key={j}>
+                              {j}
+                            </div>
+                          )}
 
-                            {isDate && <div css={C.calendarBoxTitle}>{boxIndex}</div>}
+                          {i > 0 && (
+                            <React.Fragment>
+                              {isBeforeOrToday && (
+                                <div css={[C.box, isDateBeforeCreationDate && C.boxIsBeforeCreation]}>
+                                  {Boolean(entry) && <div css={[C.entry, isDateToday && C.entryIsToday]} style={{ background: entry.label.color }} />}
 
-                            {isEntry && (
-                              <React.Fragment>
-                                {isBeforeCreationDate && (
-                                  <div css={C.calendarBoxLabelBeforeCreationIndicator} />
-                                )}
-
-                                {!isBeforeOrToday && (
-                                  <div css={C.calendarBoxLabelInactiveIndicator} />
-                                )}
-
-                                <div
-                                  css={C.calendarBoxLabel} style={Boolean(entry) ? {
-                                    background: entry.label.color
-                                  } : {}}>
-                                  {entry == null && isBeforeOrToday && (
-                                    <div
-                                      data-tite="tite"
-                                      css={[
-                                        C.calendarBoxLabelStamp,
-                                        css`
-                                              background: ${this.props.tracker.labels[this.props.activeLabelIndex].color};
-                                            `
-                                      ]}
-                                    />
+                                  {isDateToday && (
+                                    <div css={C.today}>
+                                      <div css={C.todayText}>Today</div>
+                                    </div>
                                   )}
-                                </div>
-                              </React.Fragment>
-                            )}
-                          </Content>
 
-                          {isEntryToday && <div css={C.calendarBoxContentTodayBorder} />}
+                                  <button
+                                    onClick={() => this.props.onEntryClick(i, j + 1)}
+                                    type="button"
+                                    css={C.fill}
+                                    style={isDateToday ? { top: 8 } : {}}>
+                                    <div
+                                      css={C.fillCircle}
+                                      style={{ background: tracker.labels[this.props.activeLabelIndex].color }}
+                                    />
+                                  </button>
+                                </div>
+                              )}
+
+                              {!isBeforeOrToday && <div css={[C.box, !isBeforeOrToday && C.boxIsDisabled]} />}
+                            </React.Fragment>
+                          )}
                         </div>
                       </div>
                     )
                   })}
                 </div>
-              )
-            })}
+              ))}
+            </div>
           </div>
 
           <LabelMenu {...this.props} />
